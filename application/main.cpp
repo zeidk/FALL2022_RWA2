7d0bb4516d112116fe129bae71dee3278e1cbe5b
zeidk <zeidk@umd.edu>
2022-10-16 22:50:40 -0400

    first commit

---
+++ application/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+#include "simulator/simulator.h"
+
+
+
+
+int main(int argc, char* argv[]) {
+    // colorize cell (0,0) in the simulator
+    Simulator::setColor(0, 0, 'A');
+}
